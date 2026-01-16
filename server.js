0e77be81d849ed380e91f9793f39a61305c5a28f
VipulPhatanagare <vipulphatangare3@gmail.com>
2026-01-16 09:04:31 +0530

    jyiu

--- server.js
+++ server.js
@@ -146,6 +146,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/api/activate', (req, res) => {
+  res.json({ message: 'Hello from the server!' });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
